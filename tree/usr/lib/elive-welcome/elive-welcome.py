b4305d13b39681ff279794188429153712a05e9d
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-11-12 08:36:55 -0500

    Untested - fix the issue in a "hacky" approach

--- tree/usr/lib/elive-welcome/elive-welcome.py
+++ tree/usr/lib/elive-welcome/elive-welcome.py
@@ -29,8 +29,12 @@ class EliveWelcome():
         with open("/etc/elive-version") as f:
             for line in f:
                 fixedLine = line.replace(":", "")
-                (key, val) = line.split()
-                d[key] = val
+                try:
+                    (key, val) = line.split()
+                except ValueError as ename:
+                    (key, val, val2) = line.split()
+                    val += " " + val2
+                config[key] = val
 #This would create (on my current version) a dictionary thus:
 #{'kernel': ' 4.19.0-5-amd64', 'date-builded': ' 2019-06-26', 'debian-version': ' buster', 'elive-version': ' 3.7.6', 'stable-release': ' no', 'elive-codename': ' alpha'}
 
